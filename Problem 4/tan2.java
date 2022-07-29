c16247fd43607138751709b54e99bbbcabbfff40
CHENG CHEN <105932025+chengchencon@users.noreply.github.com>
2022-07-29 08:53:23 -0400

    Update tan2.java
    implement the full algorithm of tan2

--- Problem 4/tan2.java
+++ Problem 4/tan2.java
@@ -1,9 +1,16 @@
 package func;
 import java.util.Scanner;
-
+import java.math.*;
+//tan(a+b)=(tana+tanb)/(1-tana*tanb)
 public class tan2 {
-    public static double tanCordic(double num1){
-        return 1;
+    public static double tanApart(double num1){
+        double out=0;
+        while(num1>0){
+            out= (out+0.010000333346667207)/(1-out*0.010000333346667207);
+            num1-=0.01;
+        }
+
+        return out;
     }
 
 
@@ -13,10 +20,21 @@ public class tan2 {
                 Scanner input = new Scanner(System.in);
                 System.out.println("Please enter your x for us to calculate tan(x) for you:");
                 double inVal = input.nextDouble();
-                System.out.println(inVal);
+                while(inVal>1.57079632679)
+                {
+                    inVal+=-1.57079632679;
+                }
+                while(inVal<-1.57079632679)
+                {
+                    inVal += 1.57079632679;
+                }
+                System.out.println(tanApart(inVal));
+                System.out.println(Math.tan(inVal));
+
             }catch(java.util.InputMismatchException e){
                 System.out.println("Please input numbers only!");
-            }
+                }
+
             }
         }
 }
